a65eded39579f3b05fbf55aa7241a5000946e56f
jjybaliguat <justinejeraldbaliguat@gmail.com>
2023-06-08 23:19:13 +0800

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import Head from 'next/head'
+import { theme } from '../../theme'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>
+          Resistor Website
+        </title>
+        <meta
+          name="viewport"
+          content="initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
